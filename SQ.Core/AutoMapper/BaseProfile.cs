115e88226020e58993e4a231fbef408b5cff4778
navy235 <shenhaijunmail@163.com>
2016-06-08 14:58:48 +0800

    add T4 core

---
+++ SQ.Core/AutoMapper/BaseProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+
+namespace SQ.Core.AutoMapper
+{
+    public abstract class BaseProfile : Profile
+    {
+        private readonly string _profileName;
+
+        protected BaseProfile(string profileName)
+        {
+            _profileName = profileName;
+        }
+
+        public override string ProfileName
+        {
+            get { return _profileName; }
+        }
+
+        protected override void Configure()
+        {
+            CreateMaps();
+        }
+
+        protected abstract void CreateMaps();
+    }
+}
---
+++ SQ.Core/DTO/DTOAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SQ.Core.DTO
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DTOAttribute : Attribute
+    {
+        private bool allow = true;
+
+        private string foreignKeyPropertyName = string.Empty;
+
+        private string foreignKeyEntityName = string.Empty;
+
+        public DTOAttribute(bool allow)
+        {
+            this.allow = allow;
+        }
+
+        public DTOAttribute(string foreignKeyPropertyName, string foreignKeyEntityName)
+        {
+            this.allow = true;
+       
+            this.foreignKeyPropertyName = foreignKeyPropertyName;
+
+            this.foreignKeyEntityName = foreignKeyEntityName;
+        }
+
+        public bool Allow
+        {
+            get
+            {
+                return allow;
+            }
+        }
+
+        public string ForeignKeyPropertyName
+        {
+            get
+            {
+                return foreignKeyPropertyName;
+            }
+        }
+
+        public string ForeignKeyEntityName
+        {
+            get
+            {
+                return foreignKeyEntityName;
+            }
+        }
+    }
+}
