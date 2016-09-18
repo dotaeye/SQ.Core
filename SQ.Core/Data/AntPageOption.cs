f133c006ba5b572676835cce2ed59608d165b4e5
shenhaijun <shenhaijunmail@163.com>
2016-09-18 16:37:50 +0800

    add id attribute

--- SQ.Core/Data/AntPageOption.cs
+++ SQ.Core/Data/AntPageOption.cs
@@ -8,6 +8,8 @@ namespace SQ.Core.Data
 {
     public class AntPageOption
     {
+        public int Id { get; set; }
+
         public int Results { get; set; }
 
         public int Page { get; set; }
